eff8ee36a035ff7228f841864ff11040d2caeec1
sudheer <sudheermishra8587@gmail.com>
2023-09-09 10:51:27 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,35 @@
+import "./Footer.css";
+
 function Footer() {
   return (
     <form>
-      <div>
+      <div className="from-header">
         <h1>Contact</h1>
         <hr />
-        <div></div>
+
+        <div class="form-group-name-email">
+          <div>
+            <label for="name">Name</label>
+            <br />
+            <input type="text" name="name" id="name" placeholder="john Doe" />
+          </div>
+          <br />
+          <div>
+            <label for="email">Email</label> <br />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@example.com"
+            />
+          </div>
+        </div>
+
+        <div class="form-group-message-btn">
+          <label for="message">Your Message</label> <br />
+          <textarea name="message" id="message"></textarea> <br />
+          <button className="form-group-btn">Submit</button>
+        </div>
       </div>
     </form>
   );
