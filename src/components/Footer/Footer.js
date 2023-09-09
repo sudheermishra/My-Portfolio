1619e7e5269c69600302c5192add0384735506ae
sudheer <sudheermishra8587@gmail.com>
2023-09-09 11:53:29 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,30 +7,39 @@ function Footer() {
         <h1>Contact</h1>
         <hr />
 
-        <div class="form-group-name-email">
-          <div>
-            <label for="name">Name</label>
+        <div className="form-group">
+          <div class="form-group-name-email">
+            <div>
+              <label for="name">Name</label>
+              <br />
+              <input type="text" name="name" id="name" placeholder="john Doe" />
+            </div>
             <br />
-            <input type="text" name="name" id="name" placeholder="john Doe" />
+            <div>
+              <label for="email">Email Address </label> <br />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="example@example.com"
+              />
+            </div>
           </div>
-          <br />
-          <div>
-            <label for="email">Email</label> <br />
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="example@example.com"
-            />
-          </div>
-        </div>
 
-        <div class="form-group-message-btn">
-          <label for="message">Your Message</label> <br />
-          <textarea name="message" id="message"></textarea> <br />
-          <button className="form-group-btn">Submit</button>
+          <div class="form-group-message">
+            <label for="message">Your Message</label> <br />
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Enter your Message here"></textarea>
+            <br />
+          </div>
         </div>
       </div>
+
+      <button type="submit" className="submit-btn">
+        Submit
+      </button>
     </form>
   );
 }
