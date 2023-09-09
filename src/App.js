0c08eb6c0f8838c4656b8a8f4d1f6f34191f7557
sudheer <sudheermishra8587@gmail.com>
2023-09-09 18:10:49 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Main from "./components/Main/Main";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Header />
       <Main />
       <About />
+      <Contact />
+      <Footer />
     </div>
   );
 }
