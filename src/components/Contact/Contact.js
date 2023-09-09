4e10f2231afc25b5e0fa9715872aac6b216a9989
sudheer <sudheermishra8587@gmail.com>
2023-09-09 14:23:14 +0530

    footer create

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,50 @@
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <section className="contact">
+      <div className="contact-header">
+        <h2>Contact</h2>
+      </div>
+      <hr />
+
+      <form>
+        <div className="form-group">
+          <div className="left-form">
+            <div className="form-name">
+              <label htmlFor="name">Name</label>
+              <br />
+              <input type="text" name="name" id="name" placeholder="john Doe" />
+            </div>
+            <div className="form-email">
+              <label htmlFor="Email">Email</label>
+              <br />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="johndoe@gmail.com"
+              />
+            </div>
+          </div>
+
+          <div className="right-form">
+            <div className="form-message">
+              <label htmlFor="message">Your Message</label> <br />
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Enter your Message here"></textarea>
+            </div>
+          </div>
+        </div>
+
+        <button type="submit" className="submit-btn">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
