17ce739fe9ca01f531ee06032e59200429efae08
sudheer <sudheermishra8587@gmail.com>
2026-02-16 13:55:36 +0530

    Changes few things in project section

--- src/components/Data/ProjectsData.js
+++ src/components/Data/ProjectsData.js
@@ -1,15 +1,13 @@
-import Textutils from "../../assets/ProjectsImage/Textutils.png";
-import Todolist from "../../assets/ProjectsImage/Todolist.png";
-import joke from "../../assets/ProjectsImage/Joke.png";
 import Portfolio from "../../assets/ProjectsImage/portfolio.png";
 import moviesearch from "../../assets/ProjectsImage/moviesearch.png";
 import shoppingcart from "../../assets/ProjectsImage/Cart.png";
+import TaskVault from "../../assets/ProjectsImage/taskvault.png";
 
 export const projectsData = [
   {
     id: 1,
 
-    title: " redux-toolkit-cart",
+    title: " Redux-Toolkit-Cart",
     projectInfo:
       "This Cart Application demonstrates my understanding of Redux Toolkit for global state management in React. I used React with Vite for fast development, implemented add-to-cart functionality using Redux slices, and enhanced user experience with toast notifications and icons.",
     techStack1: " HTML",
@@ -21,8 +19,21 @@ export const projectsData = [
     github: "https://github.com/sudheermishra/Shopping-cart-with-redux",
     liveLink: "https://vite-redux-cartapp.netlify.app/",
   },
+
   {
     id: 2,
+    title: "TaskVault – Secure MERN Task Manager",
+    projectInfo:
+      "TaskVault is a full-stack task management application built using the MERN stack. It includes secure user authentication with JWT, cookie handling, and protected routes for safe data access. The system supports complete CRUD operations including task creation, update, deletion, and bulk delete functionality. MongoDB Atlas is used for cloud database storage, and the application runs locally with a responsive and user-friendly interface.",
+    techStack1: "MongoDB",
+    techStack2: "Express.js",
+    techStack3: "React.js",
+    techStack4: "Node.js",
+    img: TaskVault,
+    github: "https://github.com/sudheermishra/taskvault",
+  },
+  {
+    id: 3,
 
     title: "Movie Search App",
     projectInfo:
@@ -36,7 +47,7 @@ export const projectsData = [
     liveLink: "https://movienamesearch.netlify.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "My-Portfolio",
     projectInfo:
       "My React-based portfolio offers an exceptional user experience with its sleek and intuitive design. It stands out with unique features that set it apart from the rest, making it a memorable showcase of my skills and work.",
@@ -47,44 +58,5 @@ export const projectsData = [
     img: Portfolio,
     github: "https://github.com/sudheermishra/My-Portfolio",
     liveLink: "https://sudheermishra.netlify.app/",
-  },
-  {
-    id: 4,
-    title: "Todo-List",
-    projectInfo:
-      "Todo List with Form Validation is a project that aims to create a user-friendly task management system.",
-    techStack1: " HTML",
-    techStack2: "CSS",
-    techStack3: "JavaScript",
-    img: Todolist,
-    github: "https://github.com/sudheermishra/Todo-and-singup-project",
-    liveLink: "https://todo-project-rho.vercel.app/",
-  },
-  {
-    id: 5,
-
-    title: "Joke-Joy",
-    projectInfo:
-      "Joke Joy is a fun project that fetches random jokes from an API to bring laughter and amusement to users. It's a lighthearted and entertaining random joke generator.",
-    techStack1: "HTML",
-    techStack2: "CSS ",
-    techStack3: "JavaScript",
-    img: joke,
-    github: "https://github.com/sudheermishra/Joke-project",
-    liveLink: "https://jokejoy.vercel.app/",
-  },
-  {
-    id: 6,
-
-    title: "Textutils",
-    projectInfo:
-      "TextUtils is a versatile text manipulation tool that offers case translation, copy, clear, and word counting functions. It also includes a stylish dark mode for enhanced user experience.",
-    techStack1: "HTML",
-    techStack2: "Bootstrap",
-    techStack3: "React",
-    techStack4: "JavaScript",
-    img: Textutils,
-    github: "https://github.com/sudheermishra/Textutils",
-    liveLink: "https://sudheer.vercel.app/",
   },
 ];
